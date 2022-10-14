c35b9c7f7f97b0bef10718268c4b4f390ee41ee3
Emre Ertürk <89454174+mr-emreerturk@users.noreply.github.com>
2022-10-14 17:42:14 -0400

    update all scrapers

---
+++ yahoo_scraper.py
@@ -0,0 +1,77 @@
+# Selenium imports
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+# Rest
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+import datetime
+import h5py
+import numpy as np
+
+# Global settings for the driver
+chrome_options = Options()
+chrome_options.add_experimental_option(
+    "detach", True
+)  # keeps driver open until manual termination
+
+
+class YahooScraper:
+    def __init__(self):
+        super().__init__()
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.cryptocurrencies_list = []
+
+    def scrape_yahoo(self):
+        """A method that returns the body of the table from finance.yahoo.com/cryptocurrencies"""
+
+        # global cryptocurrencies_list
+        # try:
+        #     mask = pd.read_hdf('/Users/emre/Documents/GitHub/crypto_scraping_project/hdf5_data.h5')
+        #     records = mask.to_records(index=False)
+        #     cryptocurrencies_list = list(records)
+        # except:
+        #     cryptocurrencies_list = []
+
+        # Checking length of table on URL
+        self.driver.get("https://finance.yahoo.com/cryptocurrencies/")
+        x = len(
+            self.driver.find_elements(
+                By.XPATH, '//*[@id="scr-res-table"]/div[1]/table/tbody/tr'
+            )
+        )
+
+        # Looping through every row on URL until last row
+        for row_n in range(1, x):
+            # date_time = ct = datetime.datetime.now().strftime("%D %T")
+
+            # Finding elements matching to headers using relative XPATH
+            date = datetime.datetime.now().strftime("%D")
+            name = self.driver.find_element(
+                By.XPATH,
+                f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{row_n}]/td[2]',
+            )  # Name
+            price = self.driver.find_element(
+                By.XPATH,
+                f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{row_n}]/td[3]',
+            )  # Price (Intraday)
+            marketcap = self.driver.find_element(
+                By.XPATH,
+                f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{row_n}]/td[6]',
+            )  # MarketCap
+            circ_supply = self.driver.find_element(
+                By.XPATH,
+                f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{row_n}]/td[10]',
+            )  # Circulating Supply
+
+            # Adding to body
+            self.cryptocurrencies_list.append(
+                [date, name.text, price.text, marketcap.text, circ_supply.text]
+            )
+
+        return self.cryptocurrencies_list
