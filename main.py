d34d607914f7f4bbfa26e300b44a85d1d4e00f97
Emre Ertürk <89454174+mr-emreerturk@users.noreply.github.com>
2022-10-20 17:23:12 -0400

    update gitignore

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#%%
 from coindesk_scraper import CoinDeskScraper
 from yahoo_scraper import YahooScraper
 from coinmarket_scraper import CoinMarketCapScraper
 import h5py
 import numpy as np
 
+#%%
 yahoo = YahooScraper()
 yahoo_data = yahoo.scrape_yahoo()
 
@@ -69,3 +71,25 @@ except:
             chunks=True,
         )
         coindesk_dataset_mask.attrs["USER"] = "Emre Ertürk"
+
+#%%
+with h5py.File(
+    "/Users/emre/Documents/GitHub/crypto_scraping_project/ZZZ.h5", "a"
+) as hdf:
+    yahoo_mask = np.array(yahoo.cryptocurrencies_list)
+    hdf["yahoo_prices"].resize(
+        (hdf["yahoo_prices"].shape[0] + yahoo_mask.shape[0]), axis=0
+    )
+    hdf["yahoo_prices"][yahoo_mask.shape[0] :] = yahoo_mask
+
+    cmc_mask = np.array(cmc.crypto_project_info)
+    hdf["dev_data"].resize((hdf["dev_data"].shape[0] + cmc_mask.shape[0]), axis=0)
+    hdf["dev_data"][cmc_mask.shape[0] :] = cmc_mask
+
+    coindesk_mask = np.array(coindesk.news_list)
+    hdf["news_data"].resize(
+        (hdf["news_data"].shape[0] + coindesk_mask.shape[0]), axis=0
+    )
+    hdf["news_data"][coindesk_mask.shape[0] :] = coindesk_mask
+
+# %%
